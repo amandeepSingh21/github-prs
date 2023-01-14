01736a52607b279c4708ea2de74f77a6f050b341
Amandeep <amandeep.saluja21@gmail.com>
2023-01-14 11:23:28 +0530

    Project Setup

---
+++ GithubPRs/GithubPRs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubPRs
+//
+//  Created by Amandeep on 14/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
